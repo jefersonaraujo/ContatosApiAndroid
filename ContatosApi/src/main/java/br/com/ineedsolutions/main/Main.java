588a69a150145cb9a1fef5f3d2d17655b0ae70a4
Jeferson Araujo de Sousa <jefersonaraujo95@gmail.com>
2018-01-15 11:42:33 -0300

    Commit

---
+++ ContatosApi/src/main/java/br/com/ineedsolutions/main/Main.java
@@ -0,0 +1,30 @@
+package br.com.ineedsolutions.main;
+
+import java.io.IOException;
+import java.net.URI;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.hibernate.Session;
+
+public class Main {
+	public static final String BASE_URI ="http://localhost:8080/api/";
+	
+	public static HttpServer startServer() {
+		final ResourceConfig rc = new ResourceConfig().packages("br.com.ineedsolutions");
+		
+		return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+	}
+	
+	public static void main(String[] args) throws IOException {
+		final HttpServer server = startServer();
+		
+		System.out.println("Jersey app started at: " + BASE_URI + "to shutdown, please enter!");
+		System.in.read();
+		
+		server.shutdown();
+	}
+	
+
+}
